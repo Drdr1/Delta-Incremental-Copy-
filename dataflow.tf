6ddeb09d6aedb29f2c2a834818b20e79e6414ef3
ahmed darder <91543865+Drdr1@users.noreply.github.com>
2026-04-19 09:59:09 +0200

    Update dataflow.tf

--- dataflow.tf
+++ dataflow.tf
@@ -1,12 +1,7 @@
 # ------------------------------------------------------------------------------
 # Data Flow Application
 # ------------------------------------------------------------------------------
-# Registers the PySpark script as a Data Flow application with Delta Lake
-# Spark config and configurable driver/executor shapes.
-
 locals {
-  # Build the positional arguments list for the script:
-  #   delta_incremental_copy.py <source_path> <dest_path> <time_column> <tenancy_column> [--lookback-days N]
   script_args_base = [
     var.source_path,
     var.dest_path,
@@ -22,23 +17,20 @@ locals {
 }
 
 resource "oci_dataflow_application" "delta_copy" {
-  compartment_id = oci_identity_compartment.this.id
+  compartment_id = var.compartment_id
   display_name   = var.dataflow_display_name
   language       = "PYTHON"
   spark_version  = var.spark_version
 
-  file_uri       = var.script_file_uri
-  logs_bucket_uri      = var.logs_bucket_uri
-  warehouse_bucket_uri = var.warehouse_bucket_uri
+  file_uri        = var.script_file_uri
+  logs_bucket_uri = var.logs_bucket_uri
 
-  # --- Driver shape ---
   driver_shape = var.driver_shape
   driver_shape_config {
     ocpus         = var.driver_shape_ocpus
     memory_in_gbs = var.driver_shape_memory_gb
   }
 
-  # --- Executor shape ---
   executor_shape = var.executor_shape
   executor_shape_config {
     ocpus         = var.executor_shape_ocpus
@@ -46,12 +38,10 @@ resource "oci_dataflow_application" "delta_copy" {
   }
   num_executors = var.num_executors
 
-  # --- Script arguments ---
   arguments = local.script_arguments
 
-  # --- Spark configuration (Delta Lake + multi-region) ---
   configuration = {
-    "spark.oracle.deltalake.version"                 = "3.1.0"
-    "spark.hadoop.fs.oci.client.multiregion.enabled"  = "true"
+    "spark.oracle.deltalake.version"                = "3.1.0"
+    "spark.hadoop.fs.oci.client.multiregion.enabled" = "true"
   }
 }
